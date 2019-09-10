0ef146f5ffaea1ec053cf75a1939922a7cc4cc77
Dolocentaur02 <samuel.shobowale@gmail.com>
2019-09-10 11:01:02 -0400

    Shipping Address

--- onje/onjeapp/models.py
+++ onje/onjeapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ShippingAddress (models.Model):
+    name = models.CharField("Full name", max_length=500,)
+    address1 = models.CharField("Address line2", max_length=500)
+    address2 = models.CharField("Address line2", max_length=500)
+    zip_code = models.CharField("ZIP/ Postal Code", max_length=10)
+    city = models.CharField("City", max_length=50)
+
+    def __str__(self):
+        return self.name
