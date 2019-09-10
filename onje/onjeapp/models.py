5067721778efa3091b380e489838fd39b24ff902
Dolocentaur02 <samuel.shobowale@gmail.com>
2019-09-10 16:01:55 -0400

    updateDB

--- onje/onjeapp/models.py
+++ onje/onjeapp/models.py
@@ -3,12 +3,22 @@ from django.db import models
 # Create your models here.
 
 
-class ShippingAddress (models.Model):
+class signIn (models.Model):
     name = models.CharField("Full name", max_length=500,)
-    address1 = models.CharField("Address line2", max_length=500)
+    address1 = models.CharField("Address line1", max_length=500)
     address2 = models.CharField("Address line2", max_length=500)
     zip_code = models.CharField("ZIP/ Postal Code", max_length=10)
     city = models.CharField("City", max_length=50)
 
     def __str__(self):
         return self.name
+
+# class BillingAddress (models.Model):
+#     name = models.CharField("Full name", max_length=500,)
+#     address1 = models.CharField("Address line1", max_length=500)
+#     address2 = models.CharField("Address line2", max_length=500)
+#     zip_code = models.CharField("ZIP/ Postal Code", max_length=10)
+#     city = models.CharField("City", max_length=50)
+
+#     def __str__(self):
+#         return self.name
--- onje/onjeapp/views.py
+++ onje/onjeapp/views.py
@@ -1,3 +1,84 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from .models import SignIn
+# from .forms import HouseForm, StudentForm
+
+
+def signIn_list(request):
+    signIns = signIn.objects.all()
+    return render(request, 'signIn_list.html', {'signIns': signIns})
+
+
+def signIn_detail(request, id):
+    signIn = signIn.objects.get(id=id)
+    return render(request, 'signIn_detail.html', {'signIn': signIn})
+
+
+def signIn_create(response):
+    if request.method == 'POST':
+        form = signInForm(request.POST)
+        if form.is_valid:
+            signIn = form.save()
+            return redirect('signIn_detail', id=signIn.id)
+    else:
+        form = signInForm()
+        return render(request, 'signIn_form.html', {'form': form})
+
+
+def signIn_update(request, id):
+    signIn = signIn.objects.get(id=id)
+    if request.method == 'POST':
+        form = signInForm(request.POST, instance=signIn)
+        if form.is_valid:
+            signIn = form.save()
+            return redirect('signIn_detail', id=signIn.id)
+    else:
+        form = signInForm(instance=signIn)
+        return render(request, 'signIn_form.html', {'form': form})
+
+
+def signIn_delete(request, id):
+    if request.method == 'POST':
+        signIn.objects.get(id=id).delete()
+    return redirect('signIn_list')
+
+
+# def student_list(request):
+#     students = Student.objects.all()
+#     return render(request, 'student_list.html', {'students': students})
+
+
+# def student_detail(request, id):
+#     student = Student.objects.get(id=id)
+#     return render(request, 'student_detail.html', {'student': student})
+
+
+# def student_create(request):
+#     if request.method == 'POST':
+#         form = StudentForm(request.POST)
+#         if form.is_valid:
+#             student = form.save()
+#             return redirect('student_detail', id=student.id)
+#     else:
+#         form = StudentForm()
+#         return render(request, 'student_form.html', {'form': form})
+
+
+# def student_update(request, id):
+#     student = Student.objects.get(id=id)
+#     if request.method == 'POST':
+#         form = StudentForm(request.POST, instance=student)
+#         if form.is_valid:
+#             student = form.save()
+#             return redirect('student_detail', id=student.id)
+#     else:
+#         form = StudentForm(instance=student)
+#         return render(request, 'student_form.html', {'form': form})
+
+
+# def student_delete(request, id):
+#     if request.method == 'POST':
+#         Student.objects.get(id=id).delete()
+#     return redirect('student_list')
